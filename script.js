bc4c926d1e7cd417b350ee5a84ae1f73d0d23822
Devatkov Alexey <devatkov.alexei@mail.ru>
2024-10-11 15:13:12 +0500

    790005

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ document.getElementById('loginButton').onclick = () => {
     console.log("Кнопка нажата"); // Проверка клика
     
     // Явно открываем окно авторизации в основном потоке
-    let loginWindow = window.open(`https://oauth.mail.ru/login?client_id=YOUR_API_ID&response_type=token&redirect_uri=${encodeURIComponent('https://desairdhawk.github.io/receiver.html')}&scope=mail`, 'Mail.ru OAuth', 'width=600,height=400');
+    let loginWindow = window.open(`https://oauth.mail.ru/login?client_id=790005&response_type=token&redirect_uri=${encodeURIComponent('https://desairdhawk.github.io/receiver.html')}&scope=mail`, 'Mail.ru OAuth', 'width=600,height=400');
     
     if (!loginWindow || loginWindow.closed || typeof loginWindow.closed === 'undefined') { 
         alert("Окно авторизации было заблокировано браузером. Разрешите всплывающие окна.");
